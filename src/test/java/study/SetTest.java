96c864b794776b35e3022d05e5862f9dab791910
JP <changjunpyo@gmail.com>
2021-08-09 20:13:25 +0900

    feat(changjunpyo): [0]- String & Set Class edu Test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,49 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+    @Test
+    @DisplayName("SET의 초기 사이즈는 3이다")
+    void size(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("set은 {1,2,3}을 포함하고 있다")
+    void contains(int input){
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("set은 1,2,3을 포함하고 4,5는 포함하고 있지 않다")
+    void contains(String input, String expected){
+        Integer i = Integer.getInteger(input);
+        Boolean o = Boolean.getBoolean(expected);
+        assertThat(numbers.contains(i)).isEqualTo(o);
+    }
+
+
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -10,4 +12,33 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+    @Test
+    void split() {
+        String[] got1 = "1,2".split(",");
+        String[] wanted1 = {"1", "2"};
+        assertThat(got1).isEqualTo(wanted1);
+
+        String[] got2 = "1".split(",");
+        String[] wanted2 = {"1"};
+        assertThat(got2).isEqualTo(wanted2);
+    }
+    @Test
+    void substring(){
+        String substr = "(1,2)".substring(1, 4);
+        assertThat(substr).isEqualTo("1,2");
+    }
+    @Test
+    void charAt(){
+        char first = "abc".charAt(0);
+        assertThat(first).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("인덱스의 범위가 실제 문자열 길이를 벗어날때 StringIndexOutOfBoundsException이 발생한다")
+    void charAt_2(){
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> "abc".charAt(3)
+                ).withMessageMatching("String index out of range: \\d+");
+    }
+
 }
